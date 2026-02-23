fec7e34094a8a6741ca2657cdd36ef45a24008c2
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-02-23 09:55:05 +0100

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+// #region global fields
+
+//#endregion
+
+// #region init
+/** Initializes the event listeners for selecting images in the dialog using the arrow keys.*/
+function init() {
+
+}
+
