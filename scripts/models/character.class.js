d19de28ae52e24cb4c2a561aa2969fe03c334e94
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-03-04 17:28:24 +0100

    vorbereitung collision - wir bauen blaue boxen

--- scripts/models/character.class.js
+++ scripts/models/character.class.js
@@ -6,35 +6,44 @@ class Character extends MovableObject {
     speed = 4;
     otherDirection = false;
 
-    IMAGES_WALKING = ImageHub.CHARACTER.walk;        
+    IMAGES_WALKING = ImageHub.CHARACTER.walk;
+    IMAGES_JUMPING = ImageHub.CHARACTER.jump;
     world;
 
     constructor() {
         super();
         this.loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         IntervalHub.startInterval(this.animate, 100);
-        IntervalHub.startInterval(this.move, 1000/60);
+        IntervalHub.startInterval(this.move, 1000 / 60);
+        IntervalHub.startInterval(this.applyGravity, 1000 / 60);
     }
 
     move = () => {
         if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-            this.x += this.speed;
+            this.moveRight();
             this.otherDirection = false;
-        }
-        else if (this.world.keyboard.LEFT && this.x > 0) { 
-            this.x -= this.speed;
+        } else if (this.world.keyboard.LEFT && this.x > 0) {
+            this.moveLeft();
             this.otherDirection = true;
+        } 
+        if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+            this.jump();   
         }
-        this.world.camera_x = -this.x +100;
+
+
+        this.world.camera_x = -this.x + 100;
     };
 
     animate = () => {
-        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-            // Walk animation
-            this.playAnimation(this.IMAGES_WALKING);
+        if (this.isAboveGround()) {
+            this.playAnimation(this.IMAGES_JUMPING);
+        } else {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                // Walk animation
+                this.playAnimation(this.IMAGES_WALKING);
+            }
         }
     };
-
-    jump() {}
 }
--- scripts/models/movable-object.class.js
+++ scripts/models/movable-object.class.js
@@ -7,6 +7,8 @@ class MovableObject {
     imageCache = {};
     currentImage = 0;
     speed = 0.15;
+    speedY = 0;
+    acceleration = 1;
 
     loadImage(path) {
         this.img = new Image();
@@ -22,17 +24,48 @@ class MovableObject {
     }
 
     moveRight() {
-        console.log("Moving right");
+        this.x += this.speed;
     }
 
     moveLeft = () => {
         this.x -= this.speed;
-    }
+    };
 
     playAnimation(images) {
         let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
+    }
+
+    applyGravity = () => {
+        //set interval
+        if (this.isAboveGround() || this.speedY > 0) {
+            this.y -= this.speedY;
+            this.speedY -= this.acceleration;
+        }
+    };
+
+    isAboveGround() {
+        return this.y < 180;
+    }
+
+    jump() {
+        this.speedY = 20;
+    }
+
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+     //collision Rectangle
+    drawFrame(ctx) {
+        if (!(this instanceof Character || this instanceof Chicken)) return;
+        ctx.beginPath();
+        ctx.lineWidth = "5";
+        ctx.strokeStyle = "blue";
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
     }
 }
