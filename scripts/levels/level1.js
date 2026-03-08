bd2f7f61e968cdaf372d1a3e94c5b863300b2410
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-03-08 15:17:17 +0100

    add bottles in level

--- scripts/levels/level1.js
+++ scripts/levels/level1.js
@@ -1,13 +1,16 @@
 let levelBkLength = 4;
 let imageWidth = 720;
 
+// Chicken 0 -> Normal Chicken   1 -> small chicken
+// Collectables -> 0 for 1 for bottle
+
 const level1 = new Level(
     [new Chicken(450, 0), new Chicken(550, 1), new Chicken(700, 0), new Endboss()], 
     [new BackgroundObject("../assets/img/5_background/layers/air.png", 0)],
     buildBackgrounds(ImageHub.BACKGROUNDSETS_L1),
     buildBackgrounds(ImageHub.BACKGROUNDSETS_L2),
     buildBackgrounds(ImageHub.BACKGROUNDSETS_L3),
-    [new Collectable(800,150,0), new Collectable(800,380,1)],
+    [new Collectable(800,150,0), new Collectable(850,381,1), new Collectable(1080,382,1), new Collectable(1260,379,1), new Collectable(1500,380,1), new Collectable(1800,377,1), new Collectable(2100,382,1)],
     [new Obstacle(ImageHub.OBSTACLES.mast, -140, 200, 250, 250)],
     imageWidth*levelBkLength
 );
